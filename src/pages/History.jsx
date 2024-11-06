1b0341e35188625757bf6dc55016c548494e226e
Hesham El Masry <heshamelmasry1992@gmail.com>
2024-11-06 12:19:50 +0100

    add react router two routes one for the history page and one for the home page main weather search

---
+++ src/pages/History.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function History() {
+    return <h1 className={"bg-green-900 text-white"}>History Page: List of recently searched cities with basic weather data</h1>;
+}
+
+export default History;
