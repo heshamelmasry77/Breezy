d47f1e046463a1e9ec7aee77cca59f254da2bcac
Hesham El Masry <heshamelmasry1992@gmail.com>
2024-11-06 18:31:00 +0200

    change the colours and use get current location weather

--- src/components/CitySearch.jsx
+++ src/components/CitySearch.jsx
@@ -88,16 +88,13 @@ const CitySearch = () => {
       onChange={handleCitySelect}
       className="w-full max-w-96 mx-auto"
     >
-      <label className="block text-lg font-semibold text-gray-800 mb-2 bg-blue-100 p-2 rounded-md shadow-sm">
-        Select a City
-      </label>
       <div className="flex items-center gap-2 mt-4">
         <div className="relative max-w-96 w-full">
           <ComboboxInput
             className="w-full rounded-md border-0 bg-white py-2 pl-3 pr-12 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm"
             onChange={handleInputChange}
             displayValue={(city) => (city ? city.name : "")}
-            placeholder="Search for a city..."
+            placeholder="🔎 Search for a city..."
           />
           {citySuggestions.length > 0 && (
             <ComboboxOptions className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
@@ -122,9 +119,21 @@ const CitySearch = () => {
           <button
             type="button"
             onClick={handleClear}
-            className="rounded-full bg-blue-600 px-2.5 py-1 text-xs font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+            className="rounded-full bg-blue-600 px-2.5 py-2 text-xs font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 flex items-center gap-1 justify-center shrink-0"
           >
-            Clear
+            <span>
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                viewBox="0 0 512 512"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M256 0c17.7 0 32 14.3 32 32V66.7C368.4 80.1 431.9 143.6 445.3 224H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H445.3C431.9 368.4 368.4 431.9 288 445.3V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V445.3C143.6 431.9 80.1 368.4 66.7 288H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H66.7C80.1 143.6 143.6 80.1 224 66.7V32c0-17.7 14.3-32 32-32zM128 256a128 128 0 1 0 256 0 128 128 0 1 0 -256 0zm128-80a80 80 0 1 1 0 160 80 80 0 1 1 0-160z"></path>
+              </svg>
+            </span>
+            <span>Current Location</span>
           </button>
         )}
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { CloudIcon, HomeIcon, ClockIcon } from "@heroicons/react/24/outline";
+import CitySearch from "./CitySearch.jsx";
 
 const Header = () => {
   return (
-    <header className="bg-blue-600 shadow-md">
+    <header className="bg-black shadow-md">
       <div className="container mx-auto px-4 py-4 flex items-center justify-between">
         {/* Logo */}
         <div className="flex items-center space-x-2 text-white">
           <CloudIcon className="h-8 w-8 text-yellow-300" aria-hidden="true" />
           <span className="text-2xl font-semibold">Breezy</span>
         </div>
+        <CitySearch />
 
         {/* Navigation Links */}
         <nav className="hidden md:flex space-x-6 text-white font-medium">
           <Link
             to="/"
-            className="flex items-center space-x-1 hover:text-yellow-300 transition"
+            className="flex items-center space-x-1 hover:text-yellow-300 transition cursor-pointer"
           >
             <HomeIcon className="h-5 w-5" aria-hidden="true" />
             <span>Home</span>
           </Link>
           <Link
             to="/history"
-            className="flex items-center space-x-1 hover:text-yellow-300 transition"
+            className="flex items-center space-x-1 hover:text-yellow-300 transition cursor-pointer"
           >
             <ClockIcon className="h-5 w-5" aria-hidden="true" />
             <span>History</span>
